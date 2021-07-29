dc3d89766d29c974bd5e072c84f7348e4fa98155
Nicholas <Nicholas@example.com>
2021-07-29 21:48:48 +0800

    学了一天webpack之后

---
+++ webpDemo/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "less-loader",
+            options: {
+              lessOptions: (loaderContext) => {
+                // More information about available properties https://webpack.js.org/api/loaders/
+                const { resourcePath, rootContext } = loaderContext;
+                const relativePath = path.relative(rootContext, resourcePath);
+
+                if (relativePath === "styles/foo.less") {
+                  return {
+                    paths: ["absolute/path/c", "absolute/path/d"],
+                  };
+                }
+
+                return {
+                  paths: ["absolute/path/a", "absolute/path/b"],
+                };
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        dependency: { not: ['url'] },
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+        type: "javascript/auto",
+      },
+    ],
+  },
+};
